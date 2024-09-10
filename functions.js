5a3c76b0114bc12c7e467941dd0acf4c581c9ac7
Manminder <ManminderSingh@edu.sait.ca>
2024-09-10 13:07:12 -0600

    git 10k files issue is solved and Functions repo

---
+++ functions.js
@@ -0,0 +1,16 @@
+// function = A section of code that performs a specific task and can be reused
+// Functions can take parameters and return values
+// Functions are defined using the function keyword
+// Functions are called using the function name followed by parentheses
+// Functions can be defined using function declarations, function expressions, or arrow functions
+// call the function to execute the code inside the function
+
+
+// Function Declaration
+
+function HappyBirthday() {
+  console.log("Happy Birthday! to you");
+  console.log("Happy Birthday! to you");
+      
+  console.log("Happy Birthday! to you");
+}
