09f42f6df10e42cdb2ec360e3a5f9909c4080663
Manminder <ManminderSingh@edu.sait.ca>
2024-09-10 17:39:35 -0600

    FUNCTIONS

--- functions.js
+++ functions.js
@@ -11,6 +11,87 @@
 function HappyBirthday() {
   console.log("Happy Birthday! to you");
   console.log("Happy Birthday! to you");
-      
   console.log("Happy Birthday! to you");
+  console.log("Happy Birthday! to you");
+
+  
 }
+
+HappyBirthday();
+
+//calling the function with arguments
+
+
+function greet(name , age) {
+  console.log("Hello, " + name + "you are!"  + age + " years old");
+}
+
+greet("Manminder" , 20);
+
+// donot forget to call the function
+// do not switch the order of the parameters
+
+
+// return statement in functions
+
+function add(a, b) {
+let result = a + b;
+
+  return result;    
+}
+
+let sum = add(2, 3);
+
+
+
+function multiply(a, b) {
+    let result = a * b;
+    return result;
+    }   
+    let product = multiply(2, 3);
+    console.log(product); 
+
+
+    function subtract(a, b) {
+        let result = a - b;
+        return result;
+        }   
+        let difference = subtract(2, 3);
+
+
+        function divide(a, b) {
+            let result = a / b;
+            return result;
+            };
+
+//checking odd or even number
+
+function isEVEN(num) {
+    num = Number()    //converts the value to a number
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let check = isEVEN(4); //true
+console.log(check);
+
+
+
+// CHECKING EMAIL VALIDITY
+
+function isValidEmail(email) {
+    if (email.includes("@") && email.includes(".")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let emailCheck = isValidEmail("manminders802@gmail.com");
+console.log(emailCheck); //true
+
+
+    
