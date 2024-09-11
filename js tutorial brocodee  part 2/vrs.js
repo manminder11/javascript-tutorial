f245b247dc3751f55aa5469807dff8fe27b18b7c
Manminder <ManminderSingh@edu.sait.ca>
2024-09-10 18:49:30 -0600

    variable scope

---
+++ js tutorial brocodee  part 2/vrs.js
@@ -0,0 +1,11 @@
+// variable scope is the context in which a variable is declared.
+
+// In JavaScript, scope is the set of variables, objects, and functions you have access to.
+
+// JavaScript has two scopes – global and local.
+
+// A variable declared outside a function belongs to the global scope, and is therefore accessible from anywhere in your code.
+
+// Each function has its own scope, and any variable declared within that function is only accessible from that function and any nested functions.
+
+
